5c948c5f868e87a1502b4422574c8c55f8132758
JoshLai09 <157977903+JoshLai09@users.noreply.github.com>
2024-04-28 20:30:29 +0100

    Add files via upload

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+# Tic Tac Toe
+Made using HTML + CSS and JS as a fun side project. Enjoy!
+
+## Objective
+Get three of your pieces in a row, column, or diagonal before the other player does.
+
+## Screenshots
+<img src="desktop.png" height="200" width="auto">
+<img src="mobile.png" height="200" width="auto">
+
+## Demos
+Codepen: [https://codepen.io/joshualai/pen/gOyybvr](https://codepen.io/joshualai/pen/gOyybvr)
---
+++ src/script.js
@@ -0,0 +1,67 @@
+let board = [null, null, null, null, null, null, null, null];
+let squares = Array.from(document.getElementsByClassName("square"));
+let turn = false;
+let moveCount = 0;
+
+
+function checkState() {
+  if (moveCount < 5) return "in progress";
+  let combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (const combo of combinations) {
+    const [a, b, c] = combo;
+    if (board[a] !== null && board[a] === board[b] && board[a] === board[c]) {
+      return board[a] ? "O" : "X";
+    }
+  }
+  if (moveCount === 9) return "Draw";
+  else return "in progress";
+}
+
+
+function move(ev) {
+  if (ev.target.hasChildNodes()) return;
+  if (turn == false) ev.target.innerHTML = "<span class='x'>X</span>";
+  else ev.target.innerHTML = "<span class='o'>O</span>";
+
+  board[squares.indexOf(ev.target)] = turn;
+  moveCount++;
+  turn = !turn;
+  document.getElementById("board").dataset.turn = turn.toString();
+
+  if (checkState() !== "in progress") {
+    for (let i = 0; i < squares.length; i++) {
+      squares[i].removeEventListener("click", move);
+      squares[i].style.pointerEvents = "none";
+    }
+    document.getElementById("text").innerHTML =
+    checkState() + (checkState() !== "Draw" ? " wins!" : "");
+  }
+}
+
+
+function restart() {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].innerHTML = "";
+    squares[i].addEventListener("click", move, { once: true });
+    squares[i].style.pointerEvents = "auto";
+  }
+  document.getElementById("text").innerHTML = "In progress";
+  board = [null, null, null, null, null, null, null, null];
+  moveCount = 0;
+  turn = false;
+  document.getElementById("board").dataset.turn = turn.toString();
+}
+
+
+for (let i = 0; i < squares.length; i++) {
+squares[i].addEventListener("click", move, { once: true });
+}
